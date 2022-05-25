cc7f8686516eb701e49437999520b3ffb09d593f
Soren Zaiser <soren.zaiser@gmail.com>
2022-05-25 00:41:29 -0400

    preset buttons

--- BusGui/frontend/src/App.tsx
+++ BusGui/frontend/src/App.tsx
@@ -3,7 +3,7 @@ import {Box, Container, Grid, Typography} from "@mui/material";
 import LightControl from "./Views/LightControl";
 import {useState} from "preact/hooks";
 import Dashboard from "./Views/Dashboard";
-import {Dashboard as DashboardIcon, Light} from "@mui/icons-material";
+import {AirportShuttle, Dashboard as DashboardIcon, DirectionsCar, DoorFront, Hotel, Light} from "@mui/icons-material";
 
 enum Views {
   Lights,
@@ -14,6 +14,16 @@ enum Views {
 export default function App() {
 
   const [currentView, setCurrentView] = useState<Views>(Views.Main)
+
+  const sxBase = {
+    ml: "auto",
+    border: "solid 5px white",
+    width: "45vw",
+    borderRadius: "10px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
 
   return (
     <Container sx={{width: "100vw", height: "95vh", m: 0, p: 0}}>
@@ -26,40 +36,72 @@ export default function App() {
         }
         {currentView === Views.Main &&
           <>
+            {/* Title*/}
             <Box sx={{textAlign: "center"}}>
               <Typography variant={"h4"}>BSOD the Bus Control Center</Typography>
             </Box>
+
+            {/* Lights/Dashboard Buttons */}
             <Grid container direction={"row"}>
               <Grid
                 item
-                sx={{
-                  border: "solid 5px white",
-                  paddingTop: "20%",
-                  width: "45vw",
-                  height: "80vh",
-                  borderRadius: "10px",
-                  textAlign: "center"
-                }}
+                sx={{ ...sxBase, height: "60vh" }}
                 onClick={() => setCurrentView(Views.Lights)}
               >
-                <Light fontSize={"large"} />
-                <Typography>Control Lights</Typography>
+                <Box sx={{textAlign: "center"}}>
+                  <Light fontSize={"large"} />
+                  <Typography>Control Lights</Typography>
+                </Box>
               </Grid>
               <Grid
                 item
-                sx={{
-                  ml: "auto",
-                  paddingTop: "20%",
-                  border: "solid 5px white",
-                  width: "45vw",
-                  height: "80vh",
-                  borderRadius: "10px",
-                  textAlign: "center"
-                }}
+                sx={{ ...sxBase, height: "60vh" }}
                 onClick={() => setCurrentView(Views.Dashboard)}
               >
-                <DashboardIcon fontSize={"large"} />
-                <Typography>View Dashboard</Typography>
+                <Box sx={{textAlign: "center"}}>
+                  <DashboardIcon fontSize={"large"} />
+                  <Typography>View Dashboard</Typography>
+                </Box>
+              </Grid>
+            </Grid>
+
+            {/* Scene Selection Viewer Top Row*/}
+            <Grid container direction={"row"} sx={{my: 1}}>
+              <Grid
+                item
+                sx={{ ...sxBase, height: "10vh"}}
+                onClick={() => setCurrentView(Views.Lights)}
+              >
+                <AirportShuttle sx={{mr: 2}} />
+                <Typography>Main Presets</Typography>
+              </Grid>
+              <Grid
+                item
+                sx={{ ...sxBase, height: "10vh" }}
+                onClick={() => setCurrentView(Views.Dashboard)}
+              >
+                <DirectionsCar sx={{mr: 2}} />
+                <Typography>Driver Presets</Typography>
+              </Grid>
+            </Grid>
+
+            {/* Scene Selection Viewer Bottom Row*/}
+            <Grid container direction={"row"} sx={{my: 1}}>
+              <Grid
+                item
+                sx={{ ...sxBase, height: "10vh" }}
+                onClick={() => setCurrentView(Views.Lights)}
+              >
+                <DoorFront sx={{mr: 2}} />
+                <Typography>Entry Presets</Typography>
+              </Grid>
+              <Grid
+                item
+                sx={{ ...sxBase, height: "10vh" }}
+                onClick={() => setCurrentView(Views.Dashboard)}
+              >
+                <Hotel sx={{mr: 2}} />
+                <Typography>Bedroom Presets</Typography>
               </Grid>
             </Grid>
           </>
